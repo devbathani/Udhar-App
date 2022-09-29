fb8394f828d9abf71a355b902bc9a2f79712589f
devbathani <devbathani@credicxo.com>
2022-09-29 17:12:26 +0530

    Set up completed for Udhar app

---
+++ lib/presentation/splash-screen/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Splash Screen"),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
