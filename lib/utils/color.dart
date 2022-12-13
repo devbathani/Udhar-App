fd76491806abdb29be24fc509d8a8f89522c34f5
devbathani <devbathani@credicxo.com>
2022-12-13 12:40:45 +0530

    Splash screen completed

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const pinkColor = Color(0xffF2709C);
 const lightPinkColor = Color(0xffFCC8C8);
+const greyColor = Color(0xffA8A8A8);
---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:udhar_app/utils/color.dart';
+
+final headingStyle = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 25.sp,
+    fontWeight: FontWeight.bold,
+  ),
+);
+final subHeadingStyle = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w300,
+  ),
+);
+final subTitleStyle = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    color: pinkColor,
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w500,
+  ),
+);
+
